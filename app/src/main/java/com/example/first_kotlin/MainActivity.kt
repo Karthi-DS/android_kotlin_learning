9e7ea1726ecebf26cfa801b18984e629dff77921
kartheesvaran <kartheesvaranronaldo@gmail.com>
2026-01-30 12:12:31 +0530

    add some elements

--- app/src/main/java/com/example/first_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/first_kotlin/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.first_kotlin
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val gender = resources.getStringArray(R.array.gender)
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,gender)
+        val dropdown = findViewById<Spinner>(R.id.gender)
+        dropdown.adapter = adapter;
     }
 }
